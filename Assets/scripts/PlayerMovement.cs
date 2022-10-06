15ac12b11d1286f9d2b7f7ed7c662435b0edc76b
rvst312 <115166784+rvst312@users.noreply.github.com>
2022-10-06 18:27:49 +0200

    subir proyecto

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    public float speed = 10;
+    public float rotationSpeed = 10;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        if (vertical > 0)
+        {
+            rb.AddForce(transform.up * vertical * speed * Time.deltaTime);
+            anim.SetBool("Impulsing", true);
+        }
+        else
+        {
+            anim.SetBool("Impulsing", false);
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, horizontal * rotationSpeed * Time.deltaTime);
+    }
+}
